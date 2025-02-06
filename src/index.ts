96b1b7d174f6664d95ba6eae87961f7ec4839365
Joaquín Solari Costa <joaquinsolaricosta@192.168.1.5>
2025-02-06 17:51:26 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*
+
+Older versions of the file by Mosh (then he updated content and code in the same file:
+
+console.log('Hello, world!'); //this is a comment. This is JavaScript language
+
+*/
+
+let age: number = 20;
