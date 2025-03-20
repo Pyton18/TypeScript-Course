cc306732e8cba2bab3b716762e7fd91c9e3dfe7c
Pyton18 <167722846+Pyton18@users.noreply.github.com>
2025-03-20 20:12:48 -0300

    Remove duplicate hello-world folder when I started coding from Lenovo
    Habia agregado esta carpeta sin querer al conectar la lenovo con el repositorio. Ahora lo estoy borrando

--- hello-world/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(0);
-if (customer !== null && customer !== undefined) {
-    console.log(customer.birthday);
-}
-//# sourceMappingURL=index.js.map
